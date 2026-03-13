2895f46f9ed6a0e0f910eb1c70d6bd39d8eea7e5
Suharsh Sutar <sutarsuharsh@gmail.com>
2026-03-14 00:12:38 +0530

    javascriptmodified again

---
+++ javascriptFile.js
@@ -0,0 +1,9 @@
+let variableNum = 0
+function showVariable(){
+  console.log(`Cart quantity: ${variableNum}`);
+}
+
+function addItemsToCart(){
+    variableIncreased = variableNum + 1;
+    console.log(variableIncreased);
+}
