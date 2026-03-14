4a499d233417640474c426f8e98302c28cdd31f4
Suharsh Sutar <sutarsuharsh@gmail.com>
2026-03-14 17:27:48 +0530

    final calculator look editing with javascript and html, css

--- javascriptFile.js
+++ javascriptFile.js
@@ -1,24 +1,128 @@
-let variableNum = 0
-function showVariable(){
-  console.log(`Cart quantity: ${variableNum}`);
-  addItemsToCart(variableNum);
+let calculation = "";
+let result = " ";
+
+let inputValue = document.querySelector('#inputNumber');
+
+console.log(inputValue)
+// console.log(inputAttribute);
+
+function buttonOne(){
+  calculation = `1`
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+
 }
 
-
-// console.log(variableIncreased);
-function addItemsToCart(variableNum){
-    let variableIncreased = variableNum + 1;
-  console.log(`Cart quantity: ${variableIncreased}`)
+function buttonTwo(){
+  calculation = `2`
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
 }
 
-console.log(addItemsToCart());
-
-function addTwoItemsToCart(variableIncreased){
- 
+function buttonThree(){
+  calculation = `3`
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
 }
 
-function addThreeItemsToCart(variableAddedByTwo){
-   
-    }
+function buttonFour(){
+  calculation = `4`
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
 
-    console.log(addThreeItemsToCart());
+function buttonFive(){
+  calculation = `5`
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
+
+function buttonSix(){
+  calculation = `6`
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
+
+function buttonSeven(){
+  calculation = `7`
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
+
+function buttonEight(){
+  calculation = `8`
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
+
+function buttonNine(){
+  calculation = `9`;
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
+
+function buttonZero(){
+  calculation = `0`
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
+
+function buttonPlus(){
+  calculation = ` + `
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
+
+function buttonMinus(){
+  calculation = ` - `;
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
+
+function buttonMultipe(){
+  calculation = ` * `;
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
+
+function buttonDivide(){
+  calculation = ` / `
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
+
+function buttonPercent(){
+  calculation = ` % `
+    console.log(calculation);
+    result += calculation;
+    inputValue.innerText += calculation;
+}
+
+function buttonClear(){
+  calculation = '';
+  console.log(calculation);
+  result = ''
+  inputValue.innerText = calculation;
+}
+
+function buttonSum(){
+   let finalResult = eval(result);
+    console.log(result);
+    console.log(finalResult);
+    inputValue.innerText = finalResult;
+}
+
