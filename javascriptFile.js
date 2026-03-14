05842b1e111c67e0de8f921d8e39e81f360e8168
Suharsh Sutar <sutarsuharsh@gmail.com>
2026-03-14 12:32:20 +0530

    this is edited javascript file

--- javascriptFile.js
+++ javascriptFile.js
@@ -1,9 +1,24 @@
 let variableNum = 0
 function showVariable(){
   console.log(`Cart quantity: ${variableNum}`);
+  addItemsToCart(variableNum);
 }
 
-function addItemsToCart(){
-    variableIncreased = variableNum + 1;
-    console.log(variableIncreased);
+
+// console.log(variableIncreased);
+function addItemsToCart(variableNum){
+    let variableIncreased = variableNum + 1;
+  console.log(`Cart quantity: ${variableIncreased}`)
 }
+
+console.log(addItemsToCart());
+
+function addTwoItemsToCart(variableIncreased){
+ 
+}
+
+function addThreeItemsToCart(variableAddedByTwo){
+   
+    }
+
+    console.log(addThreeItemsToCart());
